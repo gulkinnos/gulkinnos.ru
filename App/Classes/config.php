b182458480b5702cf7299a3a5fbb813e54675e3c
Aleksandr Golubev aka gulkinnos <gulkinnos@gmail.com>
2017-03-19 18:12:46 +0300

    config

--- App/Classes/config.php
+++ App/Classes/config.php
@@ -1,10 +1,18 @@
 <?php
 
-return $config= array(
+return $config = array(
     'db' => array(
         'host' => '127.0.0.1',
         'database' => 'gulkinnos',
         'user' => 'root',
         'password' => '123456789'
+    ),
+    'mainMenu' => array(
+        'Главная' => '',
+        'О проекте' => 'aboutProject/',
+        'Об авторе и соавторах' => 'aboutAuthors/',
+        'История проекта и разработчиков' => 'history/',
+        'Основные темы' => 'mainSubjects/',
+        'Заметки' => 'devNotes/'
     )
 );
