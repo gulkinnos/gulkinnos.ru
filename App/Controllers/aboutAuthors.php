0cb72cc1492599f9ab3a42bd99e65b40faddb475
Aleksandr Golubev aka gulkinnos <gulkinnos@gmail.com>
2017-03-25 18:47:24 +0300

    Добавлен контроллер и вьюха aboutAuthors

---
+++ App/Controllers/aboutAuthors.php
@@ -0,0 +1,7 @@
+<?php
+
+$headTitle = 'Про авторов проекта gulkinnos.ru';
+$headMetaDescription = 'Про авторов проекта gulkinnos.ru';
+
+include_once './templates/commonFrame.php';
+include_once './templates/aboutAuthors.php';
